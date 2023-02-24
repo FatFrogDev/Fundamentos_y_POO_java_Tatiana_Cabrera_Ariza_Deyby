61d7d16ba1ec57ce7a97b1f9e28a9eea34fdbddf
FatFrogDev <125670549+FatFrogDev@users.noreply.github.com>
2023-02-24 00:49:20 -0500

    Update and rename Trimestre I/ejercicios/1_Ejercicio_peso_animal_Ariza_deybymain.java to Trimestre I/ejercicios/17_02_2023/ejercicio1.java

--- Trimestre I/ejercicios/17_02_2023/ejercicio1.java
+++ Trimestre I/ejercicios/17_02_2023/ejercicio1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class main{
+public class ejercicio1{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Escriba el nombre del animal");
