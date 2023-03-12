825b3eaecb649f6b0f0cfcc5fbac4a5d1ad5bcb0
FatFrogDev <125670549+FatFrogDev@users.noreply.github.com>
2023-03-12 14:07:20 -0500

    Update reto1.java

--- Trimestre I/retos/reto1.java
+++ Trimestre I/retos/reto1.java
@@ -1,30 +1,15 @@
 import java.util.Scanner;
-public class reto1.java
+public class reto1
 {
 	public static void main(String[] args) {
 		int tempIn;
-		String nombre;
-		int controlador, cenToF, farToC;
+		double farToC=0;
 		Scanner tempScan = new Scanner(System.in);
-		System.out.println("Hola, ¿Qué quieres convertir?");
-		System.out.println("1. c a f ,o, 2.f a c?");
-		controlador = tempScan.nextInt();
-		
-		if(controlador==1){
-		    System.out.println("Hola, digita la temp en grados c");
-		    tempIn = tempScan.nextInt();
-		    cenToF = tempIn*9/5+32;
-		    System.out.println("La temperatura en f es: " + cenToF);
-		}
-		else if(controlador==2){
+		System.out.println("Hola, la temperatura en grados es 350°");
 		    System.out.println("Hola, digita la temp en grados f");
 		    tempIn = tempScan.nextInt();
-		    farToC=(tempIn-32)*5/9;
-		    System.out.println("La temperatura en c es: " + farToC);
-		}
-		else{
-		    System.out.println("Error");
-		}
-		tempScan.close();
+			farToC=(tempIn-32)/1.8;
+		    System.out.println("La temperatura en C° es: " + farToC);
+	tempScan.close();
 	}
 }
