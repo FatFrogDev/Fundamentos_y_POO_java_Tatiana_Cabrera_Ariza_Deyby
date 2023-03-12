f578ceec524d5205ab10c4ce2e050b787338a523
FatFrogDev <125670549+FatFrogDev@users.noreply.github.com>
2023-03-12 13:29:49 -0500

    Add files via upload

---
+++ Trimestre I/ejercicios/07_03_2023/src/ejercicio2.java
@@ -0,0 +1,22 @@
+public class ejercicio2{
+    public static void main(String[] args){
+        String name = "Usuario";
+        String rep = "Reparación total";
+        int valor = 9000;
+        System.out.println("Bienvenido al sistema para calcular el valor del arreglo del automóvil");
+        System.out.println("A continuación, te mostraremos el listado de valores");
+        System.out.println("1. Llantas = 1500");
+        System.out.println("2. Motor = 2000");
+        System.out.println("3. Pintura = 2500");
+        System.out.println("4. Golpes = 3000");
+        System.out.println("");
+        System.out.println("El total opciones es de:");
+        System.out.println("1+2 = 3500");
+        System.out.println("1+3 = 4000");
+        System.out.println("1+4 = 4500");
+        System.out.println("2+3 = 4500");
+        System.out.println("2+4 = 5000"); 
+        System.out.println("3+4 = 5500");
+        System.out.println("Hola, " + name + " el valor total de la reparación: " + rep + ", es de: " + valor + ".");
+    }
+}
